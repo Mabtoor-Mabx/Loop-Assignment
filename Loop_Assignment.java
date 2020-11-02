0b656d9129d71f76a4cf8afddbbb482eb648e19e
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2020-11-02 21:08:50 +0500

    Add files via upload

---
+++ Loop_Assignment.java
@@ -0,0 +1,77 @@
+package com.company;
+public class Loop_Assignment {
+    public static void main(String[] args) {
+        // Problem No 1
+       /* int n = 4;
+        for(int i=n; i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }*/
+
+        // Problem No 2
+/*
+        int sum = 0;
+        int n= 5;
+        int i = 0;
+        while(i<n){
+            sum = sum + (2*i);
+            i++;
+        }
+        System.out.println("The Sum of Even No is:" + sum);*/
+
+        // Problem No 3
+
+        /*int num= 2;
+        for (int i=1; i<=10;i++){
+            System.out.println(num + " x " + i + " = " + num*i);
+        }*/
+
+        // Problem No 4
+
+       /* int no = 8;
+        int i =10;
+        while(i>=1){
+            System.out.println(no + " x " + i + " = " + no*i);
+            i--;
+        }*/
+
+        // Problem No 5
+
+       /* int no = 5;
+        int fac = 1;
+        for(int i= 1 ; i<=no; i++){
+            fac*=i;
+        }
+        System.out.println(fac);*/
+
+
+        // Problem no 6
+        /*int no = 3;
+        int fac = 1;
+        int i = 1;
+        while(i<=no){
+            fac*=i;
+            i++;
+        }
+        System.out.println(fac);*/
+
+
+        // Problem No 7
+
+     /*   int n = 8;
+        int sum = 1;
+        for( int i=1; i<=10;i++){
+            sum+=n*i;
+        }
+        System.out.println(sum);*/
+// Problem No 8
+        int sum = 0;
+        int n= 5;
+        for (int i = 0; i<n; i++){
+            sum = sum + (2*i);
+        }
+        System.out.println("The Sum of Even No is:" + sum);
+    }
+}
